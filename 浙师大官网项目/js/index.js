bc939cf99d96acbc3613b65b1ea5d7d123629232
unknown <jody1225@qq.com>
2019-11-08 14:06:25 +0800

    new

---
+++ 浙师大官网项目/js/index.js
@@ -0,0 +1,6 @@
+function bc() {                //搜索框取得焦点时触发的事件
+    document.getElementById("serch").style.width = '300px';
+}
+function lk() {               //搜索框失去焦点时触发的事件
+    document.getElementById("serch").style.width = '160px';
+}
